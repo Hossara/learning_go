205ab9f173df2255310c06ecfc0a252efa7f545e
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-08-20 01:41:39 +0330

    Arrays, function, variables, slices

--- main.go
+++ main.go
@@ -3,7 +3,34 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 )
+
+type User struct {
+	firstName string
+	lastName  string
+}
+
+type car struct {
+	// inherit all user fields
+	User
+
+	make   string
+	model  string
+	height string
+	width  string
+	wheel  struct {
+		radius   int
+		material string
+	}
+}
+
+func (car car) detail() string {
+	return car.make + " " + car.model + " " + car.height + " " + car.width
+}
+
+type UserDetail struct {
+}
 
 func sayHello(name string) {
 	fmt.Printf("Hello, %s!\n", name)
@@ -17,7 +44,7 @@ func concatName(firstname, lastname string) string {
 	return fmt.Sprintf("%s %s", firstname, lastname)
 }
 
-func devide(dividend, divisor int) (int, error) {
+func devider(dividend, divisor int) (int, error) {
 	if divisor == 0 {
 		return 0, errors.New("divisor is zero")
 	}
@@ -26,6 +53,21 @@ func devide(dividend, divisor int) (int, error) {
 }
 
 func main() {
+	myCar := car{
+		User: User{
+			firstName: "James",
+		},
+		make:   "od",
+		model:  "tesla",
+		height: "12",
+		width:  "12",
+		wheel: struct {
+			radius   int
+			material string
+		}{radius: 12, material: "test"},
+	}
+
+	fmt.Printf(myCar.detail())
 	var age = 12
 
 	firstName, lastName := "Hossein", "Araghi"
@@ -43,4 +85,56 @@ func main() {
 	_, _, sum_value := sum(2, 4)
 
 	fmt.Printf("%d\n", sum_value)
+
+	//var userData User = User{firstName, lastName}
+
+	user := User{firstName, lastName}
+
+	log.Println(user)
+
+	if lenght := len(firstName); lenght < 30 {
+		fmt.Printf("Hello %d\n", lenght)
+	}
+
+	x := []string{
+		"hi",
+		"hi2",
+	}
+
+	for i := 0; i < len(x); i++ {
+		fmt.Println(x[i])
+	}
+
+	for idx, val := range x {
+		fmt.Printf("%v\t%v\n", idx, val)
+	}
+
+	for _, val := range x {
+		fmt.Printf("%v\n", val)
+	}
+
+	myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+
+	fmt.Println(myslice2)
+
+	arr1 := [5]int{
+		1: 10,
+		2: 40,
+	}
+
+	fmt.Println(arr1)
+
+	/*
+		In the example below myslice is a slice with length 2. It is made from arr1 which is an array with length 6.
+
+		The slice starts from the third element of the array which has value 12 (remember that array indexes start at 0. That means that [0] is the first element, [1] is the second element, etc.). The slice can grow to the end of the array. This means that the capacity of the slice is 4.
+
+		If myslice started from element 0, the slice capacity would be 6.
+	*/
+	arr2 := [6]int{10, 11, 12, 13, 14, 15}
+	myslice1 := arr2[2:4]
+
+	fmt.Printf("myslice = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
 }
